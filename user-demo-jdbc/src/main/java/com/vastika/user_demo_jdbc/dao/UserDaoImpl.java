2666760a426f2fb9d45253e4b6a0fddeb02e7a4c
Bhusan <43221027+bhusan1@users.noreply.github.com>
2020-08-05 22:02:29 -0500

    Add files via upload

---
+++ user-demo-jdbc/src/main/java/com/vastika/user_demo_jdbc/dao/UserDaoImpl.java
@@ -0,0 +1,125 @@
+package com.vastika.user_demo_jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.vastika.user_demo_jdbc.model.User;
+import com.vastika.user_demo_jdbc.util.DBUtil;
+import com.vastika.user_demo_jdbc.util.QueryUtil;
+
+//database interacting codes
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public int saveUserInfo(User user) {
+
+		int saved = 0;
+
+		try (
+				Connection con = DBUtil.getConnection();
+				PreparedStatement ps = con.prepareStatement(QueryUtil.INSERT_SQL);) {
+			ps.setString(1, user.getUsername());
+			ps.setString(2, user.getPassword());
+			ps.setLong(3, user.getMobileNo());
+			ps.setString(4, user.getAddress());
+			saved = ps.executeUpdate();
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return saved;
+	}
+
+	@Override
+	public int updateUserInfo(User user) {
+		
+		int updated = 0;
+
+		try (
+				Connection con = DBUtil.getConnection();
+				PreparedStatement ps = con.prepareStatement(QueryUtil.UPDATE_SQL);) {
+			ps.setString(1, user.getUsername());
+			ps.setString(2, user.getPassword());
+			ps.setLong(3, user.getMobileNo());
+			ps.setString(4, user.getAddress());
+			ps.setInt(5, user.getId());
+			updated = ps.executeUpdate();
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return updated;
+	}
+
+	@Override
+	public void deleteUserInfo(int id) {
+		try (
+				Connection con = DBUtil.getConnection();
+				PreparedStatement ps = con.prepareStatement(QueryUtil.DELETE_SQL);) {
+			ps.setInt(1, id);
+			ps.executeUpdate();
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public User getUserById(int id) {
+		
+		User user = new User();
+		try (
+				Connection con = DBUtil.getConnection();
+				PreparedStatement ps = con.prepareStatement(QueryUtil.GET_BY_ID_SQL);) {
+			
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				user.setId(rs.getInt("id"));
+				user.setUsername(rs.getString("user_name"));
+				user.setPassword(rs.getString("password"));
+				user.setAddress(rs.getString("address"));
+				user.setMobileNo(rs.getLong("mobile_no"));
+				
+			}
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return user;
+	}
+
+	@Override
+	public List<User> getAllUserInfo() {
+		
+		List<User> userList = new ArrayList();
+		
+		try (
+				Connection con = DBUtil.getConnection();
+				PreparedStatement ps = con.prepareStatement(QueryUtil.GET_BY_ID_SQL);) {
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				User user = new User();
+				user.setId(rs.getInt("id"));
+				user.setUsername(rs.getString("user_name"));
+				user.setPassword(rs.getString("password"));
+				user.setAddress(rs.getString("address"));
+				user.setMobileNo(rs.getLong("mobile_no"));
+				userList.add(user);
+			}
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return userList;
+	}
+
+}
---
+++ user-demo-jdbc/src/main/java/com/vastika/user_demo_jdbc/service/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.vastika.user_demo_jdbc.service;
+
+import java.util.List;
+
+import com.vastika.user_demo_jdbc.dao.UserDao;
+import com.vastika.user_demo_jdbc.dao.UserDaoImpl;
+import com.vastika.user_demo_jdbc.model.User;
+
+//actual business codes
+
+public class UserServiceImpl implements UserService {
+	
+	UserDao userDao = new UserDaoImpl();
+
+	@Override
+	public int saveUserInfo(User user) {
+		return userDao.saveUserInfo(user);
+	}
+
+	@Override
+	public int updateUserInfo(User user) {
+		return userDao.updateUserInfo(user);
+	}
+
+	@Override
+	public void deleteUserInfo(int id) {
+		userDao.deleteUserInfo(id);
+
+	}
+
+	@Override
+	public User getUserById(int id) {
+		
+		return userDao.getUserById(id);
+	}
+
+	@Override
+	public List<User> getAllUserInfo() {
+		return userDao.getAllUserInfo();
+	}
+
+}
